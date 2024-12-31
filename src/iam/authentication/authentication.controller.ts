912cba53c2318f549294ceb54f80c8d4dd9d2549
jhon-Min <minnyisay@gmail.com>
2024-12-31 17:22:03 +0630

    feat: add auth and non auth route logic decorator

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { CmsLoginDto } from './dto/auth.dto/auth.dto';
+import { Auth } from './decorators/auth.decorator';
+import { AuthType } from './enum/enum-type.enum';
 
 @Controller('')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
 
+  @Auth(AuthType.None)
   @Post('cms/login')
   async cmsLogin(@Body() cmsLoginDto: CmsLoginDto) {
     return this.authService.cmsLogIn(cmsLoginDto);
