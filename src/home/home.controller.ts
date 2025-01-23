54b8cf20667534913eae9425bb7150d51a8c425a
jhon-Min <minnyisay@gmail.com>
2025-01-23 18:28:12 +0630

    feat: created book lists api for frontend

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -2,14 +2,24 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GenresService } from '../genres/genres.service';
 import { AuthType } from 'src/iam/authentication/enum/enum-type.enum';
 import { Auth } from 'src/iam/authentication/decorators/auth.decorator';
+import { BooksService } from 'src/books/books.service';
 
 @Controller('api/v1')
 export class HomeController {
-  constructor(private readonly genreService: GenresService) {}
+  constructor(
+    private readonly genreService: GenresService,
+    private readonly bookService: BooksService,
+  ) {}
 
   @Auth(AuthType.None)
   @Get('genres')
   genreLists() {
     return this.genreService.index(1, 200);
+  }
+
+  @Auth(AuthType.None)
+  @Get('books')
+  books() {
+    return this.bookService.index(1, 100);
   }
 }
--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -3,9 +3,10 @@ import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
 import { PrismaService } from 'src/prisma.service';
 import { GenresService } from 'src/genres/genres.service';
+import { BooksService } from 'src/books/books.service';
 
 @Module({
   controllers: [HomeController],
-  providers: [HomeService, PrismaService, GenresService],
+  providers: [HomeService, PrismaService, GenresService, BooksService],
 })
 export class HomeModule {}
