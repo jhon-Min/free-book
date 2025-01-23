2537a2d6dc3139dfba09586ed61f802959c3ff20
jhon-Min <minnyisay@gmail.com>
2025-01-23 18:23:11 +0630

    feat: created book crud api for dashboard

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -3,8 +3,13 @@ import {
   Body,
   Controller,
   Post,
+  Get,
   UploadedFile,
   UseInterceptors,
+  Query,
+  Delete,
+  Param,
+  Put,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/book.dto';
@@ -13,6 +18,11 @@ import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('dashboard/books')
 export class BooksController {
   constructor(private readonly bookService: BooksService) {}
+
+  @Get()
+  index(@Query('page') page: string, @Query('perPage') perPage: string) {
+    return this.bookService.index(+page, +perPage);
+  }
 
   @Post()
   @UseInterceptors(
@@ -23,7 +33,9 @@ export class BooksController {
       fileFilter: (req, file, callback) => {
         if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
           return callback(
-            new BadRequestException('Only image files are allowed!'),
+            new BadRequestException(
+              'Only JPG, JPEG, and PNG files are allowed!',
+            ),
             false,
           );
         }
@@ -35,7 +47,40 @@ export class BooksController {
     @Body() createBookDto: CreateBookDto,
     @UploadedFile() book_profile: Express.Multer.File,
   ) {
-    console.log(book_profile);
     return this.bookService.createBook(createBookDto, book_profile);
+  }
+
+  @Put(':id')
+  @UseInterceptors(
+    FileInterceptor('book_profile', {
+      limits: { fileSize: 15 * 1024 * 1024 },
+      fileFilter: (req, file, callback) => {
+        const allowedTypes = /\/(jpg|jpeg|png)$/;
+
+        if (!file.mimetype.match(allowedTypes)) {
+          return callback(
+            new BadRequestException(
+              'Only JPG, JPEG, and PNG files are allowed!',
+            ),
+            false,
+          );
+        }
+
+        callback(null, true);
+      },
+    }),
+  )
+  updateBook(
+    @Param('id') id: string,
+    @Body() createBookDto: CreateBookDto,
+    @UploadedFile() book_profile: Express.Multer.File,
+  ) {
+    console.log(book_profile);
+    return this.bookService.updateBook(id, createBookDto, book_profile);
+  }
+
+  @Delete(':id')
+  destroy(@Param('id') id: string) {
+    return this.bookService.destroy(id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { CreateBookDto } from './dto/book.dto';
 import { imageUpload } from 'src/lib/myHelper';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class BooksService {
   constructor(private prismaService: PrismaService) {}
+
+  async index(page: number, perPage: number) {
+    const skip = (page - 1) * perPage;
+
+    const data = await this.prismaService.book.findMany({
+      skip,
+      take: +perPage,
+      orderBy: { createdAt: 'desc' },
+    });
+
+    const total = await this.prismaService.book.count();
+
+    return {
+      data,
+      paginations: {
+        totalItems: total,
+        currentPage: page,
+        totalPages: Math.ceil(total / +perPage),
+        itemsPerPage: perPage,
+      },
+    };
+  }
 
   async createBook(createBookDto: CreateBookDto, image: Express.Multer.File) {
     const { name, status, is_hot, is_new, description } = createBookDto;
@@ -22,5 +50,70 @@ export class BooksService {
         bookProfile: imgPath,
       },
     });
+  }
+
+  async updateBook(
+    id,
+    updateBookDto: CreateBookDto,
+    image: Express.Multer.File,
+  ) {
+    const { name, status, is_hot, is_new, description } = updateBookDto;
+
+    const book = await this.prismaService.book.findUnique({ where: { id } });
+
+    if (!book) {
+      throw new NotFoundException(` ID "${id}" not found`);
+    }
+
+    let imgPath = book.bookProfile;
+    if (image) {
+      try {
+        const relativePath = book.bookProfile.replace(process.env.APP_URL, '');
+        const oldImgPath = path.join(__dirname, `../../`, relativePath);
+
+        if (fs.existsSync(oldImgPath)) {
+          fs.unlinkSync(oldImgPath);
+        }
+
+        imgPath = await imageUpload(image, 'image/book-pf');
+      } catch (error) {
+        throw new BadRequestException(error);
+      }
+    }
+
+    return this.prismaService.book.update({
+      where: { id },
+      data: {
+        name,
+        description,
+        isHot: is_hot,
+        isNew: is_new,
+        status,
+        bookProfile: imgPath,
+      },
+    });
+  }
+
+  async destroy(id: string) {
+    const book = await this.prismaService.book.findUnique({ where: { id } });
+
+    if (!book) {
+      throw new NotFoundException(` ID "${id}" not found`);
+    }
+
+    if (book.bookProfile) {
+      const relativePath = book.bookProfile.replace(process.env.APP_URL, '');
+      const imgPath = path.join(__dirname, `../../`, relativePath);
+
+      if (fs.existsSync(imgPath)) {
+        fs.unlinkSync(imgPath);
+      }
+    }
+
+    const save = await this.prismaService.book.delete({ where: { id } });
+    return {
+      message: 'success',
+      data: save,
+    };
   }
 }
